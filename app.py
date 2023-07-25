ebf8076e6a953b39e8fae9280977f052ce31fbb7
André Fonseca Borges <borges.an@hotmail.com>
2023-07-25 08:26:57 -0300

    up Code

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask
 from flask_restful import Api
 from resources.hotel import Hoteis, Hotel
-from resources.usuario import User
+from resources.usuario import User, UserRegistration
 
 
 app = Flask(__name__)
@@ -19,6 +19,7 @@ def criar_banco():
 api.add_resource(Hoteis, '/hoteis')
 api.add_resource(Hotel, '/hoteis/<string:hotel_id>')
 api.add_resource(User, '/usuarios/<string:user_id>')
+api.add_resource(UserRegistration, '/cadastro')
 
 if __name__ == '__main__':
     from sql_alchemy import banco
--- resources/usuario.py
+++ resources/usuario.py
@@ -1,9 +1,9 @@
-from flask_restful import Resource
+from flask_restful import Resource, reqparse
 from models.usuario import UserModel
 
 
 class User(Resource):
-
+    # /usuarios/{user_id}
     def get(self, user_id):
         user = UserModel.find_hotel(user_id)
 
@@ -26,3 +26,31 @@ class User(Resource):
             return {'message': 'Hotel deleted successfully'}
 
         return {'message': 'Hotel not found'}
+
+
+class UserRegistration(Resource):
+    # /cadastro
+    def post(self):
+        atributos = reqparse.RequestParser()
+        atributos.add_argument(
+            'login',
+            type=str,
+            required=True, 
+            help="The field 'login' not blank"
+        )
+        atributos.add_argument(
+            'senha',
+            type=str,
+            required=True, 
+            help="The field 'senha' not blank"
+        )
+        dados = atributos.parse_args()
+
+        if UserModel.find_by_login(dados['login']):
+            return {
+                'message': f'The login *{dados["login"]}* already exists.'
+            }
+
+        user = UserModel(**dados)
+        user.save_user()
+        return {'message': 'Yser crated successfully!'}, 201  # Created
