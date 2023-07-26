2f0f107296d501523a7d7fcaa1652c91a9ac2cfd
André Fonseca Borges <borges.an@hotmail.com>
2023-07-26 07:14:29 -0300

    up Code

--- resources/usuario.py
+++ resources/usuario.py
@@ -5,12 +5,12 @@ from models.usuario import UserModel
 class User(Resource):
     # /usuarios/{user_id}
     def get(self, user_id):
-        user = UserModel.find_hotel(user_id)
+        user = UserModel.find_user(user_id)
 
         if user:
             return user.json()
 
-        return {'message': 'Hotel not found'}, 404  # Not found
+        return {'message': 'User not found'}, 404  # Not found
 
     def delete(self, user_id):
         user = UserModel.find_user(user_id)
@@ -20,12 +20,12 @@ class User(Resource):
                 user.delete_user()
             except Exception:
                 return {
-                    'message': 'An error occurred trying to delete hotel'
+                    'message': 'An error occurred trying to delete user'
                 }, 500  # Internal Server Error
 
-            return {'message': 'Hotel deleted successfully'}
+            return {'message': 'User deleted successfully'}
 
-        return {'message': 'Hotel not found'}
+        return {'message': 'User not found'}
 
 
 class UserRegistration(Resource):
@@ -35,13 +35,13 @@ class UserRegistration(Resource):
         atributos.add_argument(
             'login',
             type=str,
-            required=True, 
+            required=True,
             help="The field 'login' not blank"
         )
         atributos.add_argument(
             'senha',
             type=str,
-            required=True, 
+            required=True,
             help="The field 'senha' not blank"
         )
         dados = atributos.parse_args()
@@ -53,4 +53,4 @@ class UserRegistration(Resource):
 
         user = UserModel(**dados)
         user.save_user()
-        return {'message': 'Yser crated successfully!'}, 201  # Created
+        return {'message': 'User crated successfully!'}, 201  # Created
